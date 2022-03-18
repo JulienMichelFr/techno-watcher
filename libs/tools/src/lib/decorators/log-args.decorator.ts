baf60be206f3fba97ee0d700edc259f94f52eddd
Julien Michel <julienmichel.pro@gmail.com>
2022-03-18 11:50:21 +0100

    fix(tools): remove unusable typing for LogArgs decorator

--- libs/tools/src/lib/decorators/log-args.decorator.ts
+++ libs/tools/src/lib/decorators/log-args.decorator.ts
@@ -3,14 +3,14 @@
  * / ! \ Must be applied before any other decorators.
  */
 export function LogArgs(markerName?: string, disabled: boolean = false) {
-  return (target: unknown, propertyKey: string, descriptor: PropertyDescriptor): unknown => {
-    const originalMethod: (...args: unknown[]) => unknown = descriptor.value;
+  return (target: any, propertyKey: string, descriptor: PropertyDescriptor): any => {
+    const originalMethod: (...args: any[]) => any = descriptor.value;
 
     if (disabled) {
       return descriptor;
     }
 
-    descriptor.value = function (...args: unknown[]): unknown {
+    descriptor.value = function (...args: any[]): any {
       console.log(`--- START ${markerName || propertyKey} ---`);
       console.log({ args });
       console.log(`--- END ${markerName || propertyKey} ---`);
