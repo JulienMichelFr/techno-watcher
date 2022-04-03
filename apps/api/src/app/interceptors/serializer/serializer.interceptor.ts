f8fabae127d5c05858736f11bb58563126b2bce8
JulienMichelFr <julienmichel.pro@gmail.com>
2022-04-03 18:30:21 +0200

    feat(api): add serializer decorator and interceptor

---
+++ apps/api/src/app/interceptors/serializer/serializer.interceptor.ts
@@ -0,0 +1,32 @@
+import { CallHandler, ExecutionContext, Injectable, NestInterceptor } from '@nestjs/common';
+import { map, Observable } from 'rxjs';
+import { ClassConstructor, plainToInstance } from 'class-transformer';
+import { Paginated } from '@techno-watcher/api-models';
+
+@Injectable()
+export class SerializerInterceptor<Response, Input> implements NestInterceptor {
+  public constructor(private readonly model: ClassConstructor<Response>) {}
+
+  public intercept(context: ExecutionContext, next: CallHandler): Observable<Response | Paginated<Response>> {
+    return next.handle().pipe(
+      map((data) => {
+        if (this.isPaginated(data)) {
+          return {
+            ...data,
+            data: data.data.map((value) => this.serialize(value)),
+          };
+        }
+        return this.serialize(data);
+      })
+    );
+  }
+
+  private serialize(data: Input): Response {
+    return plainToInstance(this.model, data, { excludeExtraneousValues: true });
+  }
+
+  private isPaginated(data: Paginated<Input>): data is Paginated<Input> {
+    const keys: string[] = Object.keys(data);
+    return keys.includes('data') && keys.includes('total') && keys.includes('perPage') && keys.includes('from') && keys.includes('to');
+  }
+}
