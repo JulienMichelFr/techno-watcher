908bfb826aa52e2a192839c3b055613dc944dc7c
JulienMichelFr <julienmichel.pro@gmail.com>
2022-04-10 16:30:52 +0200

    chore(techno-watcher): update storybook options

---
+++ apps/techno-watcher/.storybook/preview.js
@@ -0,0 +1,19 @@
+export const parameters = {
+  backgrounds: {
+    default: 'light-gray',
+    values: [
+      {
+        name: 'light',
+        value: '#fff',
+      },
+      {
+        name: 'dark',
+        value: '#000',
+      },
+      {
+        name: 'light-gray',
+        value: '#ccc',
+      }
+    ],
+  },
+};
