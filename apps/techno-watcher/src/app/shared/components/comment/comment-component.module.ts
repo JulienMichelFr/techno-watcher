6d12f1ef7890a99cf6cffe5ba1fec94193380cf1
Julien Michel <julienmichel.pro@gmail.com>
2022-04-16 18:14:19 +0200

    feat(techno-watcher): use mat-card instead of custom implementation

--- apps/techno-watcher/src/app/shared/components/comment/comment-component.module.ts
+++ apps/techno-watcher/src/app/shared/components/comment/comment-component.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CommentComponent } from './comment.component';
-import {DateAgoModule} from "../../pipes/date-ago/date-ago.module";
-import {MatIconModule} from "@angular/material/icon";
-import {MatButtonModule} from "@angular/material/button";
+import { DateAgoModule } from '../../pipes/date-ago/date-ago.module';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 
 @NgModule({
   declarations: [CommentComponent],
-  imports: [CommonModule, DateAgoModule, MatIconModule, MatButtonModule],
+  imports: [CommonModule, DateAgoModule, MatIconModule, MatButtonModule, MatCardModule],
   exports: [CommentComponent],
 })
 export class CommentComponentModule {}
--- apps/techno-watcher/src/app/shared/components/post/post-component.module.ts
+++ apps/techno-watcher/src/app/shared/components/post/post-component.module.ts
@@ -5,10 +5,11 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatBadgeModule } from '@angular/material/badge';
 import { TagComponentModule } from '../tag/tag-component.module';
 import { DateAgoModule } from '../../pipes/date-ago/date-ago.module';
+import { MatCardModule } from '@angular/material/card';
 
 @NgModule({
   declarations: [PostComponent],
-  imports: [CommonModule, MatIconModule, MatBadgeModule, TagComponentModule, DateAgoModule],
+  imports: [CommonModule, MatIconModule, MatBadgeModule, TagComponentModule, DateAgoModule, MatCardModule],
   exports: [PostComponent],
 })
 export class PostComponentModule {}
