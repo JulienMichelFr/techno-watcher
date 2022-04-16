339df597238d854db14dea9ef8f725ecd7b4b2ae
Julien Michel <julienmichel.pro@gmail.com>
2022-04-16 19:11:37 +0200

    feat(techno-watcher): add ngrx

--- apps/techno-watcher/src/app/app.module.ts
+++ apps/techno-watcher/src/app/app.module.ts
@@ -8,10 +8,33 @@ import { AppRoutingModule } from './app-routing.module';
 import { NavbarModule } from './layout/navbar/navbar.module';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { PostComponentModule } from './shared/components/post/post-component.module';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [AppComponent, HomePageComponent],
-  imports: [BrowserModule, HttpClientModule, BrowserAnimationsModule, NavbarModule, AppRoutingModule, PostComponentModule],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    NavbarModule,
+    AppRoutingModule,
+    PostComponentModule,
+    StoreModule.forRoot(
+      {},
+      {
+        metaReducers: !environment.production ? [] : [],
+        runtimeChecks: {
+          strictActionImmutability: true,
+          strictStateImmutability: true,
+        },
+      }
+    ),
+    EffectsModule.forRoot([]),
+    !environment.production ? StoreDevtoolsModule.instrument() : [],
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
