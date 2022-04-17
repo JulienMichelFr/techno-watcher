b58710784d168db5315ce6882ab5ab8c0c2fddd2
Julien Michel <julienmichel.pro@gmail.com>
2022-04-17 08:15:16 +0200

    feat(techno-watcher): redirect to home after successful sign in / sign up

--- apps/techno-watcher/src/app/+state/auth/auth.effects.ts
+++ apps/techno-watcher/src/app/+state/auth/auth.effects.ts
@@ -4,10 +4,11 @@ import { Actions, createEffect, ofType } from '@ngrx/effects';
 import * as fromActions from './auth.actions';
 import { AuthService } from '../../services/auth/auth.service';
 import { catchError, map, mergeMap, of } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Injectable()
 export class AuthEffects {
-  public constructor(private readonly actions$: Actions, private authService: AuthService) {}
+  public constructor(private readonly actions$: Actions, private authService: AuthService, private router: Router) {}
 
   // eslint-disable-next-line @typescript-eslint/typedef
   public signInStart$ = createEffect(() =>
@@ -29,5 +30,15 @@ export class AuthEffects {
       // TODO Add error handling
       catchError(() => of(fromActions.signUpFail({ payload: {} })))
     )
+  );
+
+  // eslint-disable-next-line @typescript-eslint/typedef
+  public redirectToHomeOnSuccess$ = createEffect(
+    () =>
+      this.actions$.pipe(
+        ofType(fromActions.signInSuccess, fromActions.signUpSuccess),
+        map(() => this.router.navigate(['/']))
+      ),
+    { dispatch: false }
   );
 }
