feda14b8de875956dff33cf1b99dc42071a33c77
JulienMichelFr <julienmichel.pro@gmail.com>
2022-05-14 23:04:38 +0200

    feat(techno-watcher): remove unused search-bar, update navbar action button when signed in

--- apps/techno-watcher/src/app/layout/navbar/components/search-bar/search-bar.component.ts
+++
@@ -1,9 +0,0 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-
-@Component({
-  selector: 'techno-watcher-search-bar',
-  templateUrl: './search-bar.component.html',
-  styleUrls: ['./search-bar.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush,
-})
-export class SearchBarComponent {}
--- apps/techno-watcher/src/app/layout/navbar/navbar.module.ts
+++ apps/techno-watcher/src/app/layout/navbar/navbar.module.ts
@@ -4,14 +4,14 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { NavbarComponent } from './containers/navbar/navbar.component';
 import { AuthComponent } from './components/auth/auth.component';
-import { SearchBarComponent } from './components/search-bar/search-bar.component';
 import { MatInputModule } from '@angular/material/input';
-import {RouterModule} from "@angular/router";
-import {MatIconModule} from "@angular/material/icon";
+import { RouterModule } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
 
 @NgModule({
-  imports: [CommonModule, MatToolbarModule, MatButtonModule, MatInputModule, RouterModule, MatIconModule],
-  declarations: [NavbarComponent, AuthComponent, SearchBarComponent],
+  imports: [CommonModule, MatToolbarModule, MatButtonModule, MatInputModule, RouterModule, MatIconModule, MatMenuModule],
+  declarations: [NavbarComponent, AuthComponent],
   exports: [NavbarComponent],
 })
 export class NavbarModule {}
