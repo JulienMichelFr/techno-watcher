b84dad183ab6a18b42a29fa264e86eeb26ab33d4
Julien Michel <julienmichel.pro@gmail.com>
2022-05-14 23:59:41 +0200

    chore: update nx to 14.1.5 (#1)
    * chore: clear README.md
    
    * build: run linter check in CI build
    
    * chore: update nx to 14.1.5
    
    * style: fix lint issues

--- libs/api-models/jest.config.ts
+++ libs/api-models/jest.config.ts
@@ -1,4 +1,4 @@
-module.exports = {
+export default {
   displayName: 'api-models',
 
   globals: {
@@ -11,5 +11,5 @@ module.exports = {
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
   coverageDirectory: '../../coverage/libs/api-models',
-  preset: '../../jest.preset.ts',
+  preset: '../../jest.preset.js',
 };
--- libs/tools/src/lib/decorators/log-args.decorator.ts
+++ libs/tools/src/lib/decorators/log-args.decorator.ts
@@ -3,14 +3,14 @@
  * / ! \ Must be applied before any other decorators.
  */
 export function LogArgs(markerName?: string, disabled: boolean = false) {
-  return (target: any, propertyKey: string, descriptor: PropertyDescriptor): any => {
-    const originalMethod: (...args: any[]) => any = descriptor.value;
+  return (target: unknown, propertyKey: string, descriptor: PropertyDescriptor): unknown => {
+    const originalMethod: (...args: unknown[]) => unknown = descriptor.value;
 
     if (disabled) {
       return descriptor;
     }
 
-    descriptor.value = function (...args: any[]): any {
+    descriptor.value = function (...args: unknown[]): unknown {
       console.log(`--- START ${markerName || propertyKey} ---`);
       console.log({ args });
       console.log(`--- END ${markerName || propertyKey} ---`);
