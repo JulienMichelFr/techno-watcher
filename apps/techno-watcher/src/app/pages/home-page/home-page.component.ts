90d1ef50e258067c9b4ed340a9ca4f2e4ac39b63
JulienMichelFr <julienmichel.pro@gmail.com>
2022-05-19 21:32:10 +0200

    add pagination

--- apps/techno-watcher/src/app/pages/home-page/home-page.component.ts
+++ apps/techno-watcher/src/app/pages/home-page/home-page.component.ts
@@ -1,22 +1,47 @@
-import {ChangeDetectionStrategy, Component} from '@angular/core';
-import {PostService} from '../../services/post/post.service';
-import {map, Observable} from 'rxjs';
-import {Paginated, PostModel} from '@techno-watcher/api-models';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { PostService } from '../../services/post/post.service';
+import { BehaviorSubject, map, Observable, shareReplay, switchMap, tap } from 'rxjs';
+import { Paginated, PostModel } from '@techno-watcher/api-models';
+import { PageEvent } from '@angular/material/paginator';
 
 @Component({
   selector: 'techno-watcher-home-page',
   templateUrl: './home-page.component.html',
   styleUrls: ['./home-page.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class HomePageComponent {
-  private paginatedPosts$: Observable<Paginated<PostModel>> = this.postService.findPosts({ sort: 'createdAt:asc', take: 10, skip: 0, tags: [] });
+  public readonly pageSize: number = 10;
+  public readonly posts$: Observable<PostModel[]>;
+  public readonly total$: Observable<number>;
+  public readonly loading$: Observable<boolean>;
 
-  public posts$: Observable<PostModel[]> = this.paginatedPosts$.pipe(map(({ data }) => data));
+  private readonly paginatedPosts$: Observable<Paginated<PostModel>>;
+  private readonly pageSubject: BehaviorSubject<number> = new BehaviorSubject(0);
+  private readonly loadingSubject: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
 
-  public constructor(private postService: PostService) {}
+  public constructor(private postService: PostService) {
+    this.paginatedPosts$ = this.pageSubject.asObservable().pipe(
+      switchMap((pageIndex) => this.getPaginatedPosts$(pageIndex)),
+      shareReplay(1)
+    );
+    this.posts$ = this.paginatedPosts$.pipe(map(({ data }) => data));
+    this.total$ = this.paginatedPosts$.pipe(map(({ total }) => total));
+    this.loading$ = this.loadingSubject.asObservable();
+  }
 
   public trackByFn(index: number, item: PostModel): number {
     return item.id;
+  }
+
+  public handlePageChange({ pageIndex }: PageEvent): void {
+    this.pageSubject.next(pageIndex);
+  }
+
+  private getPaginatedPosts$(page: number): Observable<Paginated<PostModel>> {
+    this.loadingSubject.next(true);
+    return this.postService
+      .findPosts({ sort: 'createdAt:asc', take: 10, skip: page * this.pageSize, tags: [] })
+      .pipe(tap(() => this.loadingSubject.next(false)));
   }
 }
