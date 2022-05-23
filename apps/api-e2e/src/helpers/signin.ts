f327beaa29345fa95cd2188dd98d712116c454e7
JulienMichelFr <julienmichel.pro@gmail.com>
2022-05-23 23:21:02 +0200

    refactor: remove api-e2e project and move e2e test in api folder

--- apps/api-e2e/src/helpers/signin.ts
+++
@@ -1,7 +0,0 @@
-import { AuthResponseModel } from '@techno-watcher/api-models';
-import * as supertest from 'supertest';
-
-export async function signIn(email: string = 'user1@email.com', password: string = 'Pas$w0rd'): Promise<AuthResponseModel> {
-  const { body } = await supertest('http://localhost:3333').post('/auth/sign-in').send({ email, password }).expect(200);
-  return body;
-}
--- apps/api/e2e/posts/posts.e2e.spec.ts
+++ apps/api/e2e/posts/posts.e2e.spec.ts
@@ -1,21 +1,36 @@
 import * as supertest from 'supertest';
 import { signIn } from '../helpers/signin';
 import { seed } from './seed-posts';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
+import { Test, TestingModule } from '@nestjs/testing';
+import { AppModule } from '../../src/app/app.module';
 
 describe('Posts', () => {
   let bearerToken: string;
+  let app: INestApplication;
+
   beforeAll(async () => {
     await seed();
-    const { accessToken } = await signIn();
+
+    const moduleRef: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+
+    app = moduleRef.createNestApplication();
+    app.enableCors();
+    app.useGlobalPipes(
+      new ValidationPipe({
+        whitelist: true,
+        transform: true,
+      })
+    );
+    await app.init();
+    const { accessToken } = await signIn(app);
     bearerToken = `Bearer ${accessToken}`;
   });
 
-  /*afterAll(async () => {
-    await clearDatabase();
-  });*/
-
   it('GET /posts', async () => {
-    return supertest('http://localhost:3333')
+    return supertest(app.getHttpServer())
       .get('/posts')
       .expect(200)
       .expect((response) => {
@@ -24,7 +39,7 @@ describe('Posts', () => {
   });
 
   it('GET /posts?skip=10&take=10', async () => {
-    return supertest('http://localhost:3333')
+    return supertest(app.getHttpServer())
       .get('/posts?skip=10&take=10')
       .expect(200)
       .expect((response) => {
@@ -33,7 +48,7 @@ describe('Posts', () => {
   });
 
   it('GET /posts?sort=id:ASC', async () => {
-    return supertest('http://localhost:3333')
+    return supertest(app.getHttpServer())
       .get('/posts?sort=id:asc')
       .expect(200)
       .expect((response) => {
@@ -42,7 +57,7 @@ describe('Posts', () => {
   });
 
   it('GET /posts/13', async () => {
-    return supertest('http://localhost:3333')
+    return supertest(app.getHttpServer())
       .get('/posts/13')
       .expect(200)
       .expect((response) => {
@@ -51,7 +66,7 @@ describe('Posts', () => {
   });
 
   it('POST /posts', async () => {
-    return supertest('http://localhost:3333')
+    return supertest(app.getHttpServer())
       .post('/posts')
       .send({
         title: 'Post',
@@ -66,7 +81,7 @@ describe('Posts', () => {
   });
 
   it('POST /posts (Authenticated)', async () => {
-    return supertest('http://localhost:3333')
+    return supertest(app.getHttpServer())
       .post('/posts')
       .set('Authorization', bearerToken)
       .send({
@@ -86,7 +101,7 @@ describe('Posts', () => {
   });
 
   it('DELETE /posts/12', async () => {
-    return supertest('http://localhost:3333')
+    return supertest(app.getHttpServer())
       .delete('/posts/12')
       .expect(401)
       .expect((response) => {
@@ -95,7 +110,7 @@ describe('Posts', () => {
   });
 
   it('DELETE /posts/11 (Invalid user)', async () => {
-    return supertest('http://localhost:3333')
+    return supertest(app.getHttpServer())
       .delete('/posts/11')
       .set('Authorization', bearerToken)
       .expect(403)
@@ -105,6 +120,10 @@ describe('Posts', () => {
   });
 
   it('DELETE /posts/13 (Authorized)', async () => {
-    return supertest('http://localhost:3333').delete('/posts/13').set('Authorization', bearerToken).expect(200);
+    return supertest(app.getHttpServer()).delete('/posts/13').set('Authorization', bearerToken).expect(200);
+  });
+
+  afterAll(async () => {
+    await app.close();
   });
 });
