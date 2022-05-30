06bd8336792d95e118cea117f1c88f246a6fd05b
JulienMichelFr <julienmichel.pro@gmail.com>
2022-05-30 22:25:47 +0200

    add jwt-exception.filter.ts

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -5,10 +5,17 @@ import { UserModule } from './modules/user/user.module';
 import { AuthModule } from './modules/auth/auth.module';
 import { PrismaModule } from './modules/prisma/prisma.module';
 import { ConfigModule } from '@nestjs/config';
+import { JwtExceptionFilter } from './filters/jwt-exception/jwt-exception.filter';
+import { APP_FILTER } from '@nestjs/core';
 
 @Module({
   imports: [ConfigModule.forRoot({ isGlobal: true }), PostModule, UserModule, AuthModule, PrismaModule],
   controllers: [],
-  providers: [],
+  providers: [
+    {
+      provide: APP_FILTER,
+      useClass: JwtExceptionFilter,
+    },
+  ],
 })
 export class AppModule {}
