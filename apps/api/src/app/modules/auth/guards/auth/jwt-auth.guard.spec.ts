d8ebf7fde31d19915de79c9659ed5e3ccfea38cd
Julien Michel <julienmichel.pro@gmail.com>
2022-06-02 21:58:47 +0200

    feat(api): remove passport

---
+++ apps/api/src/app/modules/auth/guards/auth/jwt-auth.guard.spec.ts
@@ -0,0 +1,102 @@
+import { JwtAuthGuard } from './jwt-auth.guard';
+import { Reflector } from '@nestjs/core';
+import { JwtService } from '@nestjs/jwt';
+import { UserService } from '../../../user/services/user/user.service';
+import { ExecutionContext, UnauthorizedException } from '@nestjs/common';
+import { HttpArgumentsHost } from '@nestjs/common/interfaces';
+import { User } from '@prisma/client';
+import { Request } from 'express';
+
+describe('JwtAuthGuard', () => {
+  let guard: JwtAuthGuard;
+  let reflectorMock: Reflector;
+  let jwtServiceMock: JwtService;
+  let userServiceMock: UserService;
+  let context: ExecutionContext;
+  let httpArgumentsHostMock: HttpArgumentsHost;
+
+  beforeEach(() => {
+    reflectorMock = {
+      getAllAndOverride: jest.fn().mockReturnValue(false),
+    } as unknown as Reflector;
+
+    jwtServiceMock = {
+      verify: jest.fn(),
+    } as unknown as JwtService;
+
+    userServiceMock = {
+      findOne: jest.fn(),
+    } as unknown as UserService;
+
+    httpArgumentsHostMock = {
+      getRequest: jest.fn(),
+    } as unknown as HttpArgumentsHost;
+
+    context = {
+      getHandler: jest.fn(),
+      getClass: jest.fn(),
+      switchToHttp(): HttpArgumentsHost {
+        return httpArgumentsHostMock;
+      },
+    } as unknown as ExecutionContext;
+
+    guard = new JwtAuthGuard(reflectorMock, jwtServiceMock, userServiceMock);
+  });
+
+  it('should be defined', () => {
+    expect(guard).toBeDefined();
+  });
+
+  describe('canActivate()', () => {
+    it('should authorize if route is public', async () => {
+      (reflectorMock.getAllAndOverride as jest.Mock).mockReturnValue(true);
+      const result: boolean = await guard.canActivate(context);
+      expect(result).toBe(true);
+    });
+
+    it('should throw an unauthorized exception if token is missing', async () => {
+      (httpArgumentsHostMock.getRequest as jest.Mock).mockReturnValue({
+        header() {
+          return null;
+        },
+      });
+      await expect(guard.canActivate(context)).rejects.toThrow(new UnauthorizedException('Token is missing'));
+    });
+
+    it('should throw an unauthorized exception if user is not found', async () => {
+      (httpArgumentsHostMock.getRequest as jest.Mock).mockReturnValue({
+        header() {
+          return 'token';
+        },
+      });
+      (jwtServiceMock.verify as jest.Mock).mockReturnValue({ id: 1 });
+      (userServiceMock.findOne as jest.Mock).mockResolvedValue(null);
+      await expect(guard.canActivate(context)).rejects.toThrow(new UnauthorizedException('Token is invalid'));
+    });
+
+    it('should authorize if user is found', async () => {
+      (httpArgumentsHostMock.getRequest as jest.Mock).mockReturnValue({
+        header() {
+          return 'token';
+        },
+      });
+      (jwtServiceMock.verify as jest.Mock).mockReturnValue({ id: 1 });
+      (userServiceMock.findOne as jest.Mock).mockResolvedValue({ id: 1 });
+      const result: boolean = await guard.canActivate(context);
+      expect(result).toBe(true);
+    });
+
+    it('should store user in context request', async () => {
+      (httpArgumentsHostMock.getRequest as jest.Mock).mockReturnValue({
+        header() {
+          return 'token';
+        },
+      });
+      (jwtServiceMock.verify as jest.Mock).mockReturnValue({ id: 1 });
+      (userServiceMock.findOne as jest.Mock).mockResolvedValue({ id: 1 });
+      await guard.canActivate(context);
+      const r: Request & { user: User } = context.switchToHttp().getRequest();
+      expect(r.user).toEqual({ id: 1 });
+    });
+  });
+});
--- apps/api/src/app/modules/auth/guards/auth/jwt-auth.guard.ts
+++ apps/api/src/app/modules/auth/guards/auth/jwt-auth.guard.ts
@@ -1,31 +1,40 @@
-import { AuthGuard } from '@nestjs/passport';
 import { Reflector } from '@nestjs/core';
-import { ExecutionContext, Injectable, UnauthorizedException } from '@nestjs/common';
+import { CanActivate, ExecutionContext, Injectable, UnauthorizedException } from '@nestjs/common';
 import { IS_PUBLIC_KEY } from '../../decorators/public/public.decorator';
-import { Observable } from 'rxjs';
-import { TokenExpiredError } from 'jsonwebtoken';
+import { JwtService } from '@nestjs/jwt';
+import { JwtPayload } from '../../auth.type';
+import { Request } from 'express';
+import { UserService } from '../../../user/services/user/user.service';
+import { User } from '@prisma/client';
 
 @Injectable()
-export class JwtAuthGuard extends AuthGuard('jwt') {
-  public constructor(private reflector: Reflector) {
-    super();
-  }
+export class JwtAuthGuard implements CanActivate {
+  public constructor(private reflector: Reflector, private jwtService: JwtService, private userService: UserService) {}
 
-  public canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
+  public async canActivate(context: ExecutionContext): Promise<boolean> {
     const isPublic: boolean = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [context.getHandler(), context.getClass()]);
     if (isPublic) {
       return true;
     }
-    return super.canActivate(context);
+
+    const request: Request = context.switchToHttp().getRequest<Request>();
+    const authorizationHeader: string | null = request.header('Authorization');
+    if (!authorizationHeader) {
+      throw new UnauthorizedException('Token is missing');
+    }
+
+    const decodedJwt: JwtPayload = this.decodeJwt(authorizationHeader.replace('Bearer ', ''));
+
+    const user: User = await this.userService.findOne({ id: decodedJwt.id });
+    if (!user) {
+      throw new UnauthorizedException('Token is invalid');
+    }
+
+    context.switchToHttp().getRequest().user = user;
+    return true;
   }
 
-  public handleRequest<User>(err: Error, user: User, info: unknown): User {
-    if (info instanceof TokenExpiredError) {
-      throw new UnauthorizedException('Token expired');
-    }
-    if (err || !user) {
-      throw err || new UnauthorizedException();
-    }
-    return user;
+  private decodeJwt(token: string): JwtPayload {
+    return this.jwtService.verify<JwtPayload>(token);
   }
 }
